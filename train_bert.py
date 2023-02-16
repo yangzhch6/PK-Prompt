7f517f944d0d386b1c2c7d9c82cf8c3b7a4cd6e0
yangzhch6 <yangzhch6@mail2.sysu.edu.cn>
2023-02-16 23:41:57 +0800

    trainer config

--- train_bert.py
+++ train_bert.py
@@ -12,7 +12,7 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 dataset_name = "wiqa"
 dataset_path = "data/wiqa/test.jsonl"
 
-model_checkpoint = "/data1/yangzhicheng/Data/models/bert-base-uncased"
+model_checkpoint = "/home/Yangzhicheng/data/models/bert-base-uncased"
 model_name = model_checkpoint.split("/")[-1]
 model = AutoModelForMultipleChoice.from_pretrained(model_checkpoint).to(device=device)
 tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, do_lower_case=True)
@@ -23,13 +23,16 @@ dataset["dev"] = MultipleChoiceDataset(tokenizer, 'data/wiqa/dev.jsonl')
 dataset["test"] = MultipleChoiceDataset(tokenizer, 'data/wiqa/test.jsonl')
 
 args = TrainingArguments(
-    f"save/{model_name}-finetuned-{dataset_name}",
+    f"checkpoints/{model_name}-finetuned-{dataset_name}",
     evaluation_strategy = "epoch",
-    learning_rate=5e-5,
+    save_strategy = "epoch",
+    learning_rate=2e-5,
     per_device_train_batch_size=16,
     per_device_eval_batch_size=16,
-    num_train_epochs=3,
-    weight_decay=0.01
+    num_train_epochs=20,
+    weight_decay=0.01,
+    load_best_model_at_end=True,
+    metric_for_best_model="accuracy"
 )
 
 trainer = Trainer(
@@ -44,3 +47,4 @@ trainer = Trainer(
 
 trainer.train()
 predictions = trainer.predict(dataset["test"])
+print(predictions.metrics)
